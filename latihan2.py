9736b3ff4d6a3e354b2145be06a90ee4dc456fe2
fuadkumarudin <fuadkumarudin21@gmail.com>
2019-02-20 21:32:19 +0700

    file yang ke dua

---
+++ latihan2.py
@@ -0,0 +1,14 @@
+print("******latihan2************")
+print("Menampilkan bilangan, berhenti ketika bilangan 0, dan menampilkan bilangan terbesar")
+max=0
+while True:
+    a=int(input("masukan bilangan = "))
+    if max < a :
+        max = a
+    if a==0:
+        break
+print("Bilangan terbesarnya adalah = ",max)
+
+
+
+
